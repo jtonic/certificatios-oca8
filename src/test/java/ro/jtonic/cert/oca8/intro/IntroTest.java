2e604021443d83eb137be7475777d28463c872d2
Antonel Pazargic <antonel.pazargic@gmail.com>
2016-01-26 15:49:58 +0100

    switch gotchas

--- src/test/java/ro/jtonic/cert/oca8/intro/IntroTest.java
+++ src/test/java/ro/jtonic/cert/oca8/intro/IntroTest.java
@@ -1,5 +1,6 @@
 package ro.jtonic.cert.oca8.intro;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,11 +10,33 @@ import org.junit.Test;
  */
 public class IntroTest {
 
-
     @Test
     public void testLocalVariable() {
         int a_b;
 //        System.out.println(a_b); //Error:(16, 28) java: variable a_b might not have been initialized
+    }
+
+    @Test
+    public void testSwitch() throws Exception {
+        byte a = 1;
+        Assert.assertTrue(a == 1);
+        switch(a) {
+            case 1:
+                System.out.println("it's one!");
+                break;
+            default:
+                System.out.println("it's not!");
+        }
+
+        int dayOfTheWeek = 5;
+        switch(dayOfTheWeek){
+            case 0:
+                System.out.println("week day");
+            default:
+                System.out.println("other day");
+            case 6:
+                System.out.println("saturday");
+        }
     }
 
 }
